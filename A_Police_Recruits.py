95233fe78dac11055b6d798aff6cc8f41df4d586
ashenafi <ashenafiadola05@gmail.com>
2026-02-13 11:05:26 +0300

    solved one more

--- A_Police_Recruits.py
+++ A_Police_Recruits.py
@@ -1,27 +1,27 @@
 n = int(input())
 ns = list(map(int, input().split()))
 
-unt = 0
-i = 0
 m = 0
-while i < n-1:
-    print(i)
-    if ns[i] > 0 and ns[i+1] > 0:
-        k = 0
-        j = i
-        while ns[j] > 0:
-            m += ns[j]
-            j += 1
-            k += 1
-        for i in range(m):
-            if ns[j] < 0:
-                m -= 1
-            else:
-                m += 1
-            k += 1
-        i += k
-    elif ns[i] < 0 and m == 0:
-        unt += 1
-        i += 1
-        
-print(unt) 
+ns.reverse()
+if n > 1:
+    while True:
+        if len(ns) == 0 or ns[0] < 0:
+            break
+        if ns[0] > 0:
+            ns.remove(ns[0])
+
+
+    for i in range(len(ns)):
+        if m + ns[i] <= 0:
+            m += ns[i]
+        elif ns[i] >= -m:
+            m = 0
+    if m <= 0:    
+        print(-(m)) 
+    else:
+        print(0)
+else:
+    if ns[0] == -1:
+        print(1)
+    else:
+        print(0)
