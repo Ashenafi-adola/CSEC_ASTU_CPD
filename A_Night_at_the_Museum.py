10febd27c622892837945e4e958ad3e94b2f0582
ashenafi <ashenafiadola05@gmail.com>
2026-02-13 09:50:42 +0300

    solved one more problem

--- A_Night_at_the_Museum.py
+++ A_Night_at_the_Museum.py
@@ -1,7 +1,6 @@
 s = input()
 
 lets = "abcdefghijklmnopqrstuvwxyz"
-
 cur = 0
 r, l = 0, 0
 mv = 0
@@ -9,30 +8,24 @@ for i in s:
     iind = lets.index(i)
     if iind == cur:
         pass
-    if iind >= 13 and cur < 13:
-        r = len(lets[iind:26]) + len(lets[0:iind])
-        l = len(lets[iind:cur])
+    if iind > 12 and cur < 12:
+        r = len(lets[cur:iind])
+        l = len(lets[0:cur])+len(lets[iind-1:25])
         if l < r:
             mv += l
-            print('l1+')
         else:
             mv += r
-            print('r1+')
-    elif iind < 13 and cur >= 13:
+    elif iind < 12 and cur > 12:
         l = len(lets[0:iind]) + len(lets[cur:26]) 
         r = len(lets[iind: cur])
         if l < r:
             mv += l
-            print('l2+')
         else:
             mv += r
-            print('r2+')
     else:
         if cur > iind:
             mv += len(lets[iind: cur])
-            print('l')
         else:
             mv += len(lets[cur:iind])
-            print('r')
     cur = iind
 print(mv)
