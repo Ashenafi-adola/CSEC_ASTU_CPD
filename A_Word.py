ea52315bfc038160715021983c0925d988cc6b2d
Ashenafi Adola <ashenafiadola05@gmail.com>
2026-02-14 20:21:36 +0300

    Fix comparison logic for character case counts

--- A_Word.py
+++ A_Word.py
@@ -1,20 +1,14 @@
 st = input()
-
 caps = "QWERTYUIOPLKJHGFDSAZXCVBNM"
 smls = "qwertyuioplkjhgfdsazxcvbnm"
-
 c = 0
 s = 0
-
 for i in st:
     if i in caps:
         c += 1
     elif i in smls:
         s += 1
-
-if c < s:
-    print(st.lower())
-elif c == s:
+if c <= s:
     print(st.lower())
 else:
     print(st.upper())
