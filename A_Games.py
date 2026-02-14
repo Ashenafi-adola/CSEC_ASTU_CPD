f65416a29451bc51b79c757cae681f80033d95a0
Ashenafi Adola <ashenafiadola05@gmail.com>
2026-02-14 20:14:31 +0300

    Refactor A_Games.py for improved readability

--- A_Games.py
+++ A_Games.py
@@ -1,14 +1,11 @@
 n = int(input())
 main = []
-
 k = 0
 for i in range(n):
     main.append(list(map(int, input().split())))
-
 for i in main:
     for j in main:
         if j is not i:
             if i[0] == j[1]:
                 k += 1
-
 print(k)
