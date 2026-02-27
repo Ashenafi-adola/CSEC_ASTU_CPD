f9f24db75f7ce645013a81c505a34361f304fce2
ashenafi <ashenafiadola05@gmail.com>
2026-02-27 14:56:12 +0300

    solved the football problem

--- A_Football.py
+++ A_Football.py
@@ -1,11 +1,7 @@
-s = input()
-ret = "NO"
-if len(s) < 7:
-    print(ret)
-else:
-    for i in range(len(s)-6):
-        k = s[i:i+7]
-        if k.count('0') == 7 or k.count('1') == 7:
-            ret = "YES"
-            break
-    print(ret)
+from collections import Counter
+n = int(input())
+des = []
+for i in range(n):
+    des.append(input())
+counts = Counter(des)
+print(counts.most_common(1)[0][0])
