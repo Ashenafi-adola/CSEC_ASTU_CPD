965ace69b29173b50182e91ea1fcc2cbc6735d12
ashenafi <ashenafiadola05@gmail.com>
2026-02-28 11:14:31 +0300

    solved the dislike threes

--- A_Dislike_of_Threes.py
+++ A_Dislike_of_Threes.py
@@ -1,14 +1,11 @@
 t = int(input())
 for i in range(t):
     k = int(input())
-    tr = 1
-    pr = 1
-    while tr < k:
-        if pr % 3 == 0 or str(pr)[-1] == 3:
-            pr += 1
-        if pr % 3 == 0 or str(pr)[-1] == 3:
-            pr += 1
-        pr += 1
-        tr += 1
-        
-    print(pr)
+    tr = []
+    for i in range(10000):
+         j = str(i)
+         if i%3 == 0 or j[len(j) - 1] == '3':
+             continue
+         tr.append(i)
+         
+    print(tr[k-1])
--- A_Halloumi_Boxes.py
+++ A_Halloumi_Boxes.py
@@ -2,4 +2,9 @@ t = int(input())
 for i in range(t):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    
+    b = a.copy()
+    b.sort()
+    if k > 1 or a == b:
+        print("YES")
+    else:
+        print("NO")
