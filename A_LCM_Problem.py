31c7075112ecdf9dbb9bae61cbbdcef08aaaf111
ashenafi <ashenafiadola05@gmail.com>
2026-03-07 10:05:12 +0300

    solved the LCM problem

--- A_LCM_Problem.py
+++ A_LCM_Problem.py
@@ -2,10 +2,12 @@ t = int(input())
 for i in range(t):
     a, b = map(int, input().split())
     pr = True
-    for j in range(a+1, b+1):
-        if j%a == 0:
-            print(a, j)
+    for j in range(a, b+1):
+        if j*2 <= b:
+            print(j, j*2)
             pr = False
+            break
+        else:
             break
     if pr:
         print(-1,-1)
