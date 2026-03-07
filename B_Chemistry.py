eafc24c751574cddf6f83bb68fb7be331995d981
ashenafi <ashenafiadola05@gmail.com>
2026-03-07 09:53:38 +0300

    solved the chemistry

--- B_Chemistry.py
+++ B_Chemistry.py
@@ -1,1 +1,17 @@
-import sys
+t = int(input())
+for i in range(t):
+    n, k = map(int, input().split())
+    s = list(input())
+    d = ''
+    a = []
+    b = 0
+    for j in s:
+        if j not in d:
+            d += j
+            c = s.count(j)
+            if c%2 != 0:
+                b += 1
+    if max(0, b - 1) <= k:
+        print("YES")
+    else:
+        print("NO")
